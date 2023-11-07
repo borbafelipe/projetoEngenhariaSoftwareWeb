2255924b140f74e430a12865f47c305bd1a8de07
borbafelipe <149276247+borbafelipe@users.noreply.github.com>
2023-11-07 19:42:37 -0300

    Add files via upload
    modificaçoes no viewcontrol para switch case

--- viewcontrol.inc.php
+++ viewcontrol.inc.php
@@ -1,8 +1,28 @@
 <?php
-if (isset($_POST['buttonLabel'])) {
-    $buttonLabel = htmlspecialchars($_POST['buttonLabel']);
-    echo "Você clicou em " . $buttonLabel;
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['buttonLabel'])) {
+        $buttonLabel = htmlspecialchars($_POST['buttonLabel']);
+
+        switch ($buttonLabel) {
+            case 'home':
+                echo "[espaço de codigo para chamar dinamicamente algo]";
+                break;
+            case 'lista':
+                echo "Lista";
+                break;
+            case 'pesquise':
+                echo " Pesquise";
+                break;
+            case 'menu':
+                echo "Menu";
+                break;
+            default:
+                echo "Opção inválida";
+        }
+    } else {
+        echo "Nenhuma informação do botão recebida";
+    }
 } else {
-    echo "Você clicou em Botão 1 (padrão).";
+    echo "Menu";
 }
 ?>
